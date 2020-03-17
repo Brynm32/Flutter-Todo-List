60d702472f240b3f6e14c7ab15548b66a6e08e42
Bryan Montgomery <brynm32@yahoo.com>
2020-03-17 17:25:31 -0400

    setup

---
+++ lib/screens/todo_list_screen.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class ToDoListScreen extends StatefulWidget {
+  @override
+  _ToDoListScreenState createState() => _ToDoListScreenState();
+}
+
+class _ToDoListScreenState extends State<ToDoListScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
