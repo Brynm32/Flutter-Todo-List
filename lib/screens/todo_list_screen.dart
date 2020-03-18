fb871e60fda6e33fcccf471efad4c75bf4fa94f9
Bryan Montgomery <brynm32@yahoo.com>
2020-03-17 21:27:47 -0400

    task screen completed

--- lib/screens/todo_list_screen.dart
+++ lib/screens/todo_list_screen.dart
@@ -6,8 +6,75 @@ class ToDoListScreen extends StatefulWidget {
 }
 
 class _ToDoListScreenState extends State<ToDoListScreen> {
+  Widget _buildTask(int index) {
+    return Padding(
+      padding: EdgeInsets.symmetric(horizontal: 25.0),
+      child: Column(
+        children: <Widget>[
+          ListTile(
+            title: Text('Task Title'),
+            subtitle: Text(
+              'Oct 2, 2020 • High',
+            ),
+            trailing: Checkbox(
+              onChanged: (value) {
+                print(value);
+              },
+              activeColor: Theme.of(context).primaryColor,
+              value: true,
+            ),
+          ),
+          Divider(),
+        ],
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      // appBar: AppBar(
+      //   title: Text(''),
+      // ),
+      floatingActionButton: FloatingActionButton(
+        backgroundColor: Theme.of(context).primaryColor,
+        child: Icon(Icons.add),
+        onPressed: () => print('Navigate to add task screen'),
+      ),
+      body: ListView.builder(
+        padding: EdgeInsets.symmetric(vertical: 80.0),
+        itemCount: 10,
+        itemBuilder: (BuildContext context, int index) {
+          if (index == 0) {
+            return Padding(
+              padding: EdgeInsets.symmetric(horizontal: 40.0, vertical: 20.0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  Text(
+                    'My Tasks',
+                    style: TextStyle(
+                        color: Colors.black,
+                        fontSize: 40.0,
+                        fontWeight: FontWeight.bold),
+                  ),
+                  SizedBox(
+                    height: 10.0,
+                  ),
+                  Text(
+                    '1 of 10',
+                    style: TextStyle(
+                        color: Colors.grey,
+                        fontSize: 20.0,
+                        fontWeight: FontWeight.w500),
+                  ),
+                ],
+              ),
+            );
+          }
+          return _buildTask(index);
+        },
+      ),
+    );
   }
 }
